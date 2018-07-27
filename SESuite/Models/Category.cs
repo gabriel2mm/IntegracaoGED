58938ad5daf6bc4b3e90ecfcebd06ef05d3b3654
Gabriel moré <gmaia@amberleaf.com.br>
2018-07-27 11:33:40 -0300

    commite inicial

---
+++ SESuite/Models/Category.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace testeSuites.Models
+{
+    [XmlRoot(ElementName = "item", Namespace = "urn:document")]
+    public class Item
+    {
+        [XmlElement(ElementName = "CDCATEGORY", Namespace = "urn:document")]
+        public string CDCATEGORY { get; set; }
+        [XmlElement(ElementName = "CDCATEGORYOWNER", Namespace = "urn:document")]
+        public string CDCATEGORYOWNER { get; set; }
+        [XmlElement(ElementName = "IDCATEGORY", Namespace = "urn:document")]
+        public string IDCATEGORY { get; set; }
+        [XmlElement(ElementName = "NMCATEGORY", Namespace = "urn:document")]
+        public string NMCATEGORY { get; set; }
+    }
+
+    [XmlRoot(ElementName = "RESULTARRAY", Namespace = "urn:document")]
+    public class RESULTARRAY
+    {
+        [XmlElement(ElementName = "item", Namespace = "urn:document")]
+        public List<Item> Item { get; set; }
+    }
+
+    [XmlRoot(ElementName = "return", Namespace = "urn:document")]
+    public class Return
+    {
+        [XmlElement(ElementName = "RESULTARRAY", Namespace = "urn:document")]
+        public RESULTARRAY RESULTARRAY { get; set; }
+    }
+
+    [XmlRoot(ElementName = "searchCategoryResponse", Namespace = "urn:document")]
+    public class SearchCategoryResponse
+    {
+        [XmlElement(ElementName = "return", Namespace = "urn:document")]
+        public Return Return { get; set; }
+        [XmlAttribute(AttributeName = "xmlns")]
+        public string Xmlns { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class Body
+    {
+        [XmlElement(ElementName = "searchCategoryResponse", Namespace = "urn:document")]
+        public SearchCategoryResponse SearchCategoryResponse { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class Envelope
+    {
+        [XmlElement(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+        public Body Body { get; set; }
+        [XmlAttribute(AttributeName = "SOAP-ENV", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string SOAPENV { get; set; }
+        [XmlAttribute(AttributeName = "xsd", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Xsd { get; set; }
+        [XmlAttribute(AttributeName = "xsi", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Xsi { get; set; }
+        [XmlAttribute(AttributeName = "SOAP-ENC", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string SOAPENC { get; set; }
+    }
+
+}
---
+++ SESuite/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using testeSuites.Models;
+using testeSuites.WSGEDDocument;
+
+namespace testeSuites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            var _url = "https://200.186.58.10/softexpert/webserviceproxy/se/ws/dc_ws.php";
+            var _action = "";
+            string xml = @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:urn=""urn:document""><soapenv:Header/><soapenv:Body><urn:searchCategory/></soapenv:Body></soapenv:Envelope>";
+
+            string soapResult = getResponse(_url , xml, _action);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Envelope));
+            using (TextReader reader = new StringReader(soapResult))
+            {
+                Envelope result = (Envelope)serializer.Deserialize(reader);
+
+                foreach (Item i in result.Body.SearchCategoryResponse.Return.RESULTARRAY.Item)
+                {
+                    Console.WriteLine(i.CDCATEGORY);
+                    Console.WriteLine(i.NMCATEGORY);
+                    Console.WriteLine(i.CDCATEGORYOWNER);
+                    Console.WriteLine("--------------------------");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static string getResponse(String _url, String xml, String _action)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            XmlDocument soapEnvelopeXml = CreateSoapEnvelope(xml);
+            HttpWebRequest webRequest = CreateWebRequest(_url, _action);
+            InsertSoapEnvelopeIntoWebRequest(soapEnvelopeXml, webRequest);
+
+            IAsyncResult asyncResult = webRequest.BeginGetResponse(null, null);
+            asyncResult.AsyncWaitHandle.WaitOne();
+
+            string soapResult;
+            using (WebResponse webResponse = webRequest.EndGetResponse(asyncResult))
+            {
+                using (StreamReader rd = new StreamReader(webResponse.GetResponseStream()))
+                {
+                    soapResult = rd.ReadToEnd();
+                }
+            }
+
+            return soapResult;
+        }
+        private static XmlDocument CreateSoapEnvelope(string xml)
+        {
+            XmlDocument soapEnvelopeDocument = new XmlDocument();
+            soapEnvelopeDocument.LoadXml(xml);
+            return soapEnvelopeDocument;
+        }
+
+        private static HttpWebRequest CreateWebRequest(string url, string action)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Headers.Add("Authorization", "Basic aW50ZWdyYWNhbzppbnRlZ3JhY2FvQDIwMTg=");
+            webRequest.Headers.Add("SOAPAction", action);
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            return webRequest;
+        }
+
+        private static void InsertSoapEnvelopeIntoWebRequest(XmlDocument soapEnvelopeXml, HttpWebRequest webRequest)
+        {
+            using (Stream stream = webRequest.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+        }
+
+
+    }
+}
